1be49b996157d91fa5bccff5568045116b0bd043
Gerardo <gerardo.ezk.080204@gmail.com>
2026-06-29 01:15:31 -0500

    corrige referencia de region en modulo network

--- infra/modules/network/main.tf
+++ infra/modules/network/main.tf
@@ -6,6 +6,8 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+data "aws_region" "current" {}
+
 resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
